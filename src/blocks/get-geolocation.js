f30cc039967cb2c4801033e50c045ee5a35dd9ce
Bugaian Roman <roman.bugaian@altran.com>
2018-02-21 02:38:17 +0100

    added

--- src/blocks/get-geolocation.js
+++ src/blocks/get-geolocation.js
@@ -26,8 +26,34 @@ export class GetGeolocationButton extends Component {
         }
     }
 
+    onSuccess = function(position) {
+        alert('Latitude: '          + position.coords.latitude          + '\n' +
+              'Longitude: '         + position.coords.longitude         + '\n' +
+              'Altitude: '          + position.coords.altitude          + '\n' +
+              'Accuracy: '          + position.coords.accuracy          + '\n' +
+              'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+              'Heading: '           + position.coords.heading           + '\n' +
+              'Speed: '             + position.coords.speed             + '\n' +
+              'Timestamp: '         + position.timestamp                + '\n');
+    };
+
+    // onError Callback receives a PositionError object
+    //
+    onError = function (error) {
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+    }
+
+
     searchLocation() {
         console.log('searchLocation');
+
+        document.addEventListener("deviceready", onDeviceReady, false);
+        function onDeviceReady() {
+            console.log("navigator.geolocation works well");
+            navigator.geolocation.getCurrentPosition(onSuccess, onError);
+        }
+        
         try {
             let locationAccuracy = new Promise((resolve, reject) => {
                 cordova.plugins.locationAccuracy.canRequest(
